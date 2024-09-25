9f564eb3906d3d0ad51dd323122ce2d63103e794
Dhairya Prajapati <d.prajapati3430@gmail.com>
2024-09-25 17:35:35 -0600

    Added colour in shape class

--- app/src/main/java/com/example/orange/shape.java
+++ app/src/main/java/com/example/orange/shape.java
@@ -3,12 +3,12 @@ package com.example.orange;
 public abstract class shape {
     protected int x;
     protected int y;
-    String colour = "$orange";
+
 
     // Constructor to initialize x and y
     public shape(int x, int y) {
         this.x = x;
         this.y = y;
     }
-
+    String colour = "$orange";
 }
