bf37ba8d42b0d4973493b376636134bea0f079f3
RadhePatel04 <radhep04@gmail.com>
2024-09-24 19:26:51 -0600

    added colour

---
+++ app/src/main/java/com/example/orange/rectangle.java
@@ -0,0 +1,5 @@
+package com.example.orange;
+
+public class rectangle extends shape{
+
+}
--- app/src/main/java/com/example/orange/ui/rectangle.java
+++
@@ -1,7 +0,0 @@
-package com.example.orange.ui;
-
-import com.example.orange.shape;
-
-public class rectangle extends shape{
-    
-}
