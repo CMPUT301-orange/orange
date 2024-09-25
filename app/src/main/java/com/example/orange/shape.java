e5d9ddeb5db97e0de15d9f62227d649c19997631
Greyhamm <flokstraagraham@gmail.com>
2024-09-24 18:34:32 -0600

    Added Shape Class

---
+++ app/src/main/java/com/example/orange/shape.java
@@ -0,0 +1,13 @@
+package com.example.orange;
+
+public abstract class shape {
+    protected int x;
+    protected int y;
+
+    // Constructor to initialize x and y
+    public shape(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+}
