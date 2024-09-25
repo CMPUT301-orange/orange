84a4266505a857ac307e1c6296c1612eabab4284
RadhePatel04 <radhep04@gmail.com>
2024-09-25 14:46:50 -0600

    added colour

--- app/src/main/java/com/example/orange/shape.java
+++ app/src/main/java/com/example/orange/shape.java
@@ -3,12 +3,11 @@ package com.example.orange;
 public abstract class shape {
     protected int x;
     protected int y;
+    String color = "Blue";
 
     // Constructor to initialize x and y
     public shape(int x, int y) {
         this.x = x;
         this.y = y;
     }
-
-    String color = "Blue";
 }
